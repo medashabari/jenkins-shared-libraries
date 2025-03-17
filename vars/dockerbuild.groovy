2309a0a1ed5760a5f2fcf177e1bef84c7dd7fcda
Meda Shabarish <medashabari@gmail.com>
2025-03-17 10:32:03 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,3 +1,3 @@
 def call(String dockerUserName, String imageName, String tag){
-  sh "docker build -t ${dockerUserName}/${imageName}:${tag}"  
+  sh "docker build -t ${dockerUserName}/${imageName}:${tag} ."  
 }
